3803eb8d49cf1b55e7124789c74a616b96b4ff4c
Arun Prasad <anagarathinam@walmart.com>
2013-08-27 00:19:08 -0700

    OMR Added Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omr::Application.config.secret_token = '29a704ac4421b0fe8729f9019ded79e9fd0e6043600e23b2bac662a9fbb4b49d6d4f89428876465d0416917597055e99826c8561a8f3f88b29881b4a384cbdab'
